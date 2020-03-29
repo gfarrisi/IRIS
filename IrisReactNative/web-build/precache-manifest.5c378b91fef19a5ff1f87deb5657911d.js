a33c053a650aee8bdb587d4895625da523ce5e1b
gfarrisi <gabriellafarrisi@gmail.com>
2020-03-29 15:54:53 -0400

    first commit

---
+++ IrisReactNative/web-build/precache-manifest.5c378b91fef19a5ff1f87deb5657911d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "818911f54acad08a6ef4",
+    "url": "/static/js/runtime~app.86c3a96a.js"
+  },
+  {
+    "revision": "3aa1f250d6bffe78973d",
+    "url": "/static/js/app.37f7de7d.chunk.js"
+  },
+  {
+    "revision": "d26a17b562937bdc96af",
+    "url": "/static/js/2.c01dd792.chunk.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "04cf1ae10e30155ca207400239ec1cbd",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "bb815a5b1303cb8b32474a33c1679dd8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "7a4171454aaba6a5ebab63442cc5cf82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  }
+];
